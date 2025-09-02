cee5f6feb9a4d08e445cfe8e00f4567a9ed2fed8
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-02 13:51:54 +0545

    variables

--- variables/variables.py
+++ variables/variables.py
@@ -1,2 +1,24 @@
 a="hii"
 print(a)   #this prints the value of a
+
+x=3 #x is of type int 
+x="How are you doing?"   #x is now of type str
+print(x)    #this prints the latest value of x which is "How are you doing?"
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))   #this prints what data type is this variable with type() function
+
+x="what"
+#is same as
+x='what'
+
+q="thanks"
+Q="nope" #Q will not overwrite q
+#variables names are case sensitive
+
---
+++ variables/variablesnames.py
@@ -0,0 +1,29 @@
+# A variable can have a short name (like x or y or a) or more descriptive name (age, carname, total_output).
+# A variable name must start with a letter or underscore character , cannot start with a nuber .
+# A variable can only conatin alpha-numeric characters and underscores(A-z,0-9 and _)
+# Case sensitive (age,Age and AGE are three different variables)
+# Variable name cannot be any of the Python keywords
+
+#example correct way for declaring variable name
+myvar="Python"
+my_var="Python"
+_my_var="Python"
+myVar="Python"
+MYVAR="Python"
+myvar2="Python"  
+
+#exapmle incorrect variable name
+# 2myvar="Python"
+# my-var="Python"
+# my var="Python"
+
+#multi words variables name 
+# camel case-each word except first ,starts with a capital letter :
+myVariableName="Python"
+
+#pascal case-each word starts with a capital letter :
+MyVariableName="Python"
+
+#snake case-each word separated by an underscore :
+my_variable_name="Python"
+
