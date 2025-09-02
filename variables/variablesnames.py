41280433dacd3998c5594260ff84e095d08f1b68
Swosti Makaju <swosti.makaju@gmail.com>
2025-09-02 14:45:02 +0545

    variables

--- variables/variablesnames.py
+++ variables/variablesnames.py
@@ -27,3 +27,63 @@ MyVariableName="Python"
 #snake case-each word separated by an underscore :
 my_variable_name="Python"
 
+#multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)  #this prints value of x,y,z
+
+x=y=z="🍵"
+print(x) #this prints 🍵
+print(y) #this prints 🍵
+print(z) #this prints 🍵
+
+car = ["Porsche", "BMW", "Thar"]
+x, y, z = car
+print(x)   #this prints Porsche
+print(y)    #this prints BMW
+print(z)     #this prints Thar
+
+x="hello"
+y="there"
+print(x,y) #this prints "hello there" you can also use + operator like print(x+y) for multiple variables
+
+# but for numbers + works as mathematical operator 
+x=55
+y=99
+print(x+y) #this adds the value of x and y but if you try to combine a string and a number with + operator python will give you an error
+
+#  variables created outside of a function are known as global variables.
+#  global variables can be used by everyone, both inside and outside of functions
+# example
+x="Nepal"
+def myfunc():
+    print(x)
+    myfunc()
+
+# creating a variable inside a function , with same name as global variable 
+y="Nepal"
+def myfunc():
+    y="Bhaktapur"
+    print(y)
+    myfunc
+
+# to create a global variable inside a function, you can use the global keyword 
+#if you use global keyword, the variable belongs to global scope :
+def myfunc():
+    global x
+    x="nepal"
+    myfunc()
+    print(x)
+
+# to change the value of a global variable inside a function , refer to variable by using global keyword
+x="wow"
+def mnyfunc():
+    global x
+    x="nice"
+    myfunc()
+    print(x)
+
+    
+
+
