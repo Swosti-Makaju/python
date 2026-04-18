572481abefbfc33d6af4150cd4a8f8a9fe56999f
Swosti Makaju <swosti.makaju@gmail.com>
2026-04-18 21:18:41 +0545

    c2

---
+++ chapter2/average.py
@@ -0,0 +1,11 @@
+a=30
+b=32
+c=a+b
+d=c/2
+print(f"The average of {a} and {b} is: {d}")
+
+a=float(input("Enter the first number:"))
+b=float(input("Enter the second number:"))
+c=a+b
+d=c/2
+print(f"The average of {a} and {b} is: {d}")
