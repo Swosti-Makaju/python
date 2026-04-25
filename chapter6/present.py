9e585c583826c64e114014f71090e116fa514883
Swosti Makaju <swosti.makaju@gmail.com>
2026-04-25 21:13:01 +0545

    c6

---
+++ chapter6/present.py
@@ -0,0 +1,6 @@
+names=["swosti","srasss","swaraaa","siso"]
+name=input("enter name:")
+if name in names:
+    print("name is present in the list")
+else:
+    print("name is not present in the list")
