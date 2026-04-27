ca1cb969ced77f66e83abf33edb025ca56c12d0b
Swosti Makaju <swosti.makaju@gmail.com>
2026-04-27 21:00:29 +0545

    c7

---
+++ chapter7/star.py
@@ -0,0 +1,16 @@
+n = 3
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2*i - 1))
+
+
+    n = 3
+for i in range(1, n + 1):
+    print("*" * i)
+
+
+    n = 3
+for i in range(n):
+    if i == 1:
+        print("* *")
+    else:
+        print("* * *")
