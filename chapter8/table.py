8fe12c2b837ee7aadea344c2cd279be1dc5e4059
Swosti Makaju <swosti.makaju@gmail.com>
2026-04-27 21:18:07 +0545

    c8

---
+++ chapter8/table.py
@@ -0,0 +1,12 @@
+def table(n):
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n*i}")
+
+table(5)
+
+#input user
+text=input("enter a number: ")
+def table(n):
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n*i}")
+table(int(text))
