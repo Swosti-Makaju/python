1614b1256e876df6ea378cdbdf6ec54f6ec3c5fa
Swosti Makaju <swosti.makaju@gmail.com>
2026-04-28 19:38:59 +0545

    c9 table

---
+++ chapter9/table.py
@@ -0,0 +1,15 @@
+import os
+
+def generateTable(n):
+    table = ""
+    for i in range(1, 11):
+        table += f"{n} x {i} = {n*i}\n"
+
+    os.makedirs("chapter9/tables", exist_ok=True)
+
+    with open(f"chapter9/tables/table_{n}.txt", "w") as f:
+        f.write(table)
+
+
+for i in range(2, 21):
+    generateTable(i)
