45048f316eae7d383cc6726d7c6c1831c9894382
Swosti Makaju <swosti.makaju@gmail.com>
2026-05-01 21:12:22 +0545

    c13

---
+++ chapter13/p1.py
@@ -0,0 +1,6 @@
+name=input("Enter your name: ")
+marks=int(input("Enter your marks: "))  
+phone=int(input("Enter your phone number: "))
+
+s="the name of the student is {} and his marks are {} and his phone number is {}".format(name,marks,phone)
+print(s)
